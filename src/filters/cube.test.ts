6ccc7b6343e60a5785d9fe691a1668291cf1e144
Daniel Soskey <asks@danielsoskey.com>
2024-03-27 10:20:29 -0700

    fix broken test

--- src/filters/cube.test.ts
+++ src/filters/cube.test.ts
@@ -28,12 +28,12 @@ const dataProvider = new MemoryDataProvider({
 describe('cube filter', function() {
   const queryRunner = new QueryRunner({ corpus, dataProvider, defaultOptions })
   it("filters cards in an old model cube", async function() {
-    const result = names(await queryRunner.search("cube:gy"))
+    const result = names(await queryRunner.search("cubeo:gy"))
 
     expect(result).toEqual([birdsOfParadise.name, bloodCrypt.name])
   })
   it("filters cards in a new model cube", async function() {
-    const result = names(await queryRunner.search("cube:soskgy"))
+    const result = names(await queryRunner.search("cubeo:soskgy"))
 
     expect(result).toEqual([birdsOfParadise.name, bloodCrypt.name])
   })
