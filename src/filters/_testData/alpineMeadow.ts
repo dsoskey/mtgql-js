23a4101a75f1d8e41e443a8f99e68e462b142d6f
Daniel Soskey <asks@danielsoskey.com>
2025-10-11 19:53:57 -0700

    3.2.6a - add test data

---
+++ src/filters/_testData/alpineMeadow.ts
@@ -0,0 +1,138 @@
+import {Card} from "../../generated";
+
+export const alpineMeadow: Card = {
+    "object": "card",
+    "id": "8702d6b9-bb01-4841-a76d-4a576066c772",
+    "oracle_id": "8c281ebe-d9a1-48af-b58b-19c55aa4625b",
+    "multiverse_ids": [
+        503864
+    ],
+    "mtgo_id": 87839,
+    "arena_id": 75295,
+    "tcgplayer_id": 230106,
+    "cardmarket_id": 529412,
+    "name": "Alpine Meadow",
+    "lang": "en",
+    "released_at": "2021-02-05",
+    "uri": "https://api.scryfall.com/cards/8702d6b9-bb01-4841-a76d-4a576066c772",
+    "scryfall_uri": "https://scryfall.com/card/khm/248/alpine-meadow?utm_source=api",
+    "layout": "normal",
+    "highres_image": true,
+    "image_status": "highres_scan",
+    "image_uris": {
+        "small": "https://cards.scryfall.io/small/front/8/7/8702d6b9-bb01-4841-a76d-4a576066c772.jpg?1631052433",
+        "normal": "https://cards.scryfall.io/normal/front/8/7/8702d6b9-bb01-4841-a76d-4a576066c772.jpg?1631052433",
+        "large": "https://cards.scryfall.io/large/front/8/7/8702d6b9-bb01-4841-a76d-4a576066c772.jpg?1631052433",
+        "png": "https://cards.scryfall.io/png/front/8/7/8702d6b9-bb01-4841-a76d-4a576066c772.png?1631052433",
+        "art_crop": "https://cards.scryfall.io/art_crop/front/8/7/8702d6b9-bb01-4841-a76d-4a576066c772.jpg?1631052433",
+        "border_crop": "https://cards.scryfall.io/border_crop/front/8/7/8702d6b9-bb01-4841-a76d-4a576066c772.jpg?1631052433"
+    },
+    "mana_cost": "",
+    "cmc": 0.0,
+    "type_line": "Snow Land — Mountain Plains",
+    "oracle_text": "({T}: Add {R} or {W}.)\nThis land enters tapped.",
+    "colors": [],
+    "color_identity": [
+        "R",
+        "W"
+    ],
+    "keywords": [],
+    "produced_mana": [
+        "R",
+        "W"
+    ],
+    "legalities": {
+        "standard": "not_legal",
+        "future": "not_legal",
+        "historic": "legal",
+        "timeless": "legal",
+        "gladiator": "legal",
+        "pioneer": "legal",
+        "modern": "legal",
+        "legacy": "legal",
+        "pauper": "legal",
+        "vintage": "legal",
+        "penny": "not_legal",
+        "commander": "legal",
+        "oathbreaker": "legal",
+        "standardbrawl": "not_legal",
+        "brawl": "legal",
+        "alchemy": "not_legal",
+        "paupercommander": "legal",
+        "duel": "legal",
+        "oldschool": "not_legal",
+        "premodern": "not_legal",
+        "predh": "not_legal"
+    },
+    "games": [
+        "arena",
+        "paper",
+        "mtgo"
+    ],
+    "reserved": false,
+    "game_changer": false,
+    "foil": true,
+    "nonfoil": true,
+    "finishes": [
+        "nonfoil",
+        "foil"
+    ],
+    "oversized": false,
+    "promo": false,
+    "reprint": false,
+    "variation": false,
+    "set_id": "43057fad-b1c1-437f-bc48-0045bce6d8c9",
+    "set": "khm",
+    "set_name": "Kaldheim",
+    "set_type": "expansion",
+    "set_uri": "https://api.scryfall.com/sets/43057fad-b1c1-437f-bc48-0045bce6d8c9",
+    "set_search_uri": "https://api.scryfall.com/cards/search?order=set&q=e%3Akhm&unique=prints",
+    "scryfall_set_uri": "https://scryfall.com/sets/khm?utm_source=api",
+    "rulings_uri": "https://api.scryfall.com/cards/8702d6b9-bb01-4841-a76d-4a576066c772/rulings",
+    "prints_search_uri": "https://api.scryfall.com/cards/search?order=released&q=oracleid%3A8c281ebe-d9a1-48af-b58b-19c55aa4625b&unique=prints",
+    "collector_number": "248",
+    "digital": false,
+    "rarity": "common",
+    "flavor_text": "\"Here perished Rognar the Reckless after his hundred-day battle with the Ironmaw Dragon. We raised these stones to mark his resting place.\"\n—Iskene, Kannah storyteller",
+    "card_back_id": "0aeebaf5-8c7d-4636-9e82-8c27447861f7",
+    "artist": "Piotr Dura",
+    "artist_ids": [
+        "aff176e8-1d15-432e-ad1d-207a474decba"
+    ],
+    "illustration_id": "86c1ca1f-ebe0-4aec-a021-8614ef55d1e3",
+    "border_color": "black",
+    "frame": "2015",
+    "frame_effects": [
+        "snow"
+    ],
+    "full_art": false,
+    "textless": false,
+    "booster": true,
+    "story_spotlight": false,
+    "edhrec_rank": 3448,
+    "penny_rank": 784,
+    "preview": {
+        "source": "Wizards of the Coast",
+        "source_uri": "https://www.twitch.tv/videos/864684586",
+        "previewed_at": "2021-01-08"
+    },
+    "prices": {
+        "usd": "0.30",
+        "usd_foil": "0.41",
+        "usd_etched": null,
+        "eur": "0.34",
+        "eur_foil": "0.70",
+        "tix": "0.03"
+    },
+    "related_uris": {
+        "gatherer": "https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=503864&printed=false",
+        "tcgplayer_infinite_articles": "https://partner.tcgplayer.com/c/4931599/1830156/21018?subId1=api&trafcat=tcgplayer.com%2Fsearch%2Farticles&u=https%3A%2F%2Fwww.tcgplayer.com%2Fsearch%2Farticles%3FproductLineName%3Dmagic%26q%3DAlpine%2BMeadow",
+        "tcgplayer_infinite_decks": "https://partner.tcgplayer.com/c/4931599/1830156/21018?subId1=api&trafcat=tcgplayer.com%2Fsearch%2Fdecks&u=https%3A%2F%2Fwww.tcgplayer.com%2Fsearch%2Fdecks%3FproductLineName%3Dmagic%26q%3DAlpine%2BMeadow",
+        "edhrec": "https://edhrec.com/route/?cc=Alpine+Meadow"
+    },
+    "purchase_uris": {
+        "tcgplayer": "https://partner.tcgplayer.com/c/4931599/1830156/21018?subId1=api&u=https%3A%2F%2Fwww.tcgplayer.com%2Fproduct%2F230106%3Fpage%3D1",
+        "cardmarket": "https://www.cardmarket.com/en/Magic/Products?idProduct=529412&referrer=scryfall&utm_campaign=card_prices&utm_medium=text&utm_source=scryfall",
+        "cardhoarder": "https://www.cardhoarder.com/cards/87839?affiliate_id=scryfall&ref=card-profile&utm_campaign=affiliate&utm_medium=card&utm_source=scryfall"
+    }
+} as Card;
---
+++ src/filters/_testData/bonecrusherGiant.ts
@@ -0,0 +1,170 @@
+import {Card} from "../../generated";
+
+export const bonecrusherGiant: Card = {
+    "object": "card",
+    "id": "ff984a4c-1818-4f8f-a9d7-fce57e77937d",
+    "oracle_id": "d6d72f5f-8f5d-4180-b514-f22ff5482902",
+    "multiverse_ids": [],
+    "tcgplayer_id": 199232,
+    "cardmarket_id": 401229,
+    "name": "Bonecrusher Giant // Stomp",
+    "lang": "en",
+    "released_at": "2019-10-04",
+    "uri": "https://api.scryfall.com/cards/ff984a4c-1818-4f8f-a9d7-fce57e77937d",
+    "scryfall_uri": "https://scryfall.com/card/eld/291/bonecrusher-giant-stomp?utm_source=api",
+    "layout": "adventure",
+    "highres_image": true,
+    "image_status": "highres_scan",
+    "image_uris": {
+        "small": "https://cards.scryfall.io/small/front/f/f/ff984a4c-1818-4f8f-a9d7-fce57e77937d.jpg?1571367563",
+        "normal": "https://cards.scryfall.io/normal/front/f/f/ff984a4c-1818-4f8f-a9d7-fce57e77937d.jpg?1571367563",
+        "large": "https://cards.scryfall.io/large/front/f/f/ff984a4c-1818-4f8f-a9d7-fce57e77937d.jpg?1571367563",
+        "png": "https://cards.scryfall.io/png/front/f/f/ff984a4c-1818-4f8f-a9d7-fce57e77937d.png?1571367563",
+        "art_crop": "https://cards.scryfall.io/art_crop/front/f/f/ff984a4c-1818-4f8f-a9d7-fce57e77937d.jpg?1571367563",
+        "border_crop": "https://cards.scryfall.io/border_crop/front/f/f/ff984a4c-1818-4f8f-a9d7-fce57e77937d.jpg?1571367563"
+    },
+    "mana_cost": "{2}{R} // {1}{R}",
+    "cmc": 3.0,
+    "type_line": "Creature — Giant // Instant — Adventure",
+    "power": "4",
+    "toughness": "3",
+    "colors": [
+        "R"
+    ],
+    "color_identity": [
+        "R"
+    ],
+    "keywords": [],
+    "card_faces": [
+        {
+            "object": "card_face",
+            "name": "Bonecrusher Giant",
+            "mana_cost": "{2}{R}",
+            "type_line": "Creature — Giant",
+            "oracle_text": "Whenever this creature becomes the target of a spell, this creature deals 2 damage to that spell's controller.",
+            "power": "4",
+            "toughness": "3",
+            "flavor_text": "Not every tale ends in glory.",
+            "artist": "John Severin Brassell",
+            "artist_id": "a4f50310-4b01-4cd3-8ab8-85b31e6876db",
+            "illustration_id": "f2a7285c-2ba8-483a-bf52-1fd05fe73476"
+        },
+        {
+            "object": "card_face",
+            "name": "Stomp",
+            "mana_cost": "{1}{R}",
+            "type_line": "Instant — Adventure",
+            "oracle_text": "Damage can't be prevented this turn. Stomp deals 2 damage to any target.",
+            "artist": "John Severin Brassell",
+            "artist_id": "a4f50310-4b01-4cd3-8ab8-85b31e6876db"
+        }
+    ],
+    "all_parts": [
+        {
+            "object": "related_card",
+            "id": "a8d50cf9-03e6-467a-aadf-84a24106ffa9",
+            "component": "combo_piece",
+            "name": "On an Adventure",
+            "type_line": "Card",
+            "uri": "https://api.scryfall.com/cards/a8d50cf9-03e6-467a-aadf-84a24106ffa9"
+        },
+        {
+            "object": "related_card",
+            "id": "ff984a4c-1818-4f8f-a9d7-fce57e77937d",
+            "component": "combo_piece",
+            "name": "Bonecrusher Giant // Stomp",
+            "type_line": "Creature — Giant // Instant — Adventure",
+            "uri": "https://api.scryfall.com/cards/ff984a4c-1818-4f8f-a9d7-fce57e77937d"
+        }
+    ],
+    "legalities": {
+        "standard": "not_legal",
+        "future": "not_legal",
+        "historic": "legal",
+        "timeless": "legal",
+        "gladiator": "legal",
+        "pioneer": "legal",
+        "modern": "legal",
+        "legacy": "legal",
+        "pauper": "not_legal",
+        "vintage": "legal",
+        "penny": "not_legal",
+        "commander": "legal",
+        "oathbreaker": "legal",
+        "standardbrawl": "not_legal",
+        "brawl": "legal",
+        "alchemy": "not_legal",
+        "paupercommander": "not_legal",
+        "duel": "legal",
+        "oldschool": "not_legal",
+        "premodern": "not_legal",
+        "predh": "not_legal"
+    },
+    "games": [
+        "paper",
+        "mtgo"
+    ],
+    "reserved": false,
+    "game_changer": false,
+    "foil": true,
+    "nonfoil": true,
+    "finishes": [
+        "nonfoil",
+        "foil"
+    ],
+    "oversized": false,
+    "promo": false,
+    "reprint": false,
+    "variation": false,
+    "set_id": "a90a7b2f-9dd8-4fc7-9f7d-8ea2797ec782",
+    "set": "eld",
+    "set_name": "Throne of Eldraine",
+    "set_type": "expansion",
+    "set_uri": "https://api.scryfall.com/sets/a90a7b2f-9dd8-4fc7-9f7d-8ea2797ec782",
+    "set_search_uri": "https://api.scryfall.com/cards/search?order=set&q=e%3Aeld&unique=prints",
+    "scryfall_set_uri": "https://scryfall.com/sets/eld?utm_source=api",
+    "rulings_uri": "https://api.scryfall.com/cards/ff984a4c-1818-4f8f-a9d7-fce57e77937d/rulings",
+    "prints_search_uri": "https://api.scryfall.com/cards/search?order=released&q=oracleid%3Ad6d72f5f-8f5d-4180-b514-f22ff5482902&unique=prints",
+    "collector_number": "291",
+    "digital": false,
+    "rarity": "rare",
+    "flavor_text": "Not every tale ends in glory.",
+    "card_back_id": "0aeebaf5-8c7d-4636-9e82-8c27447861f7",
+    "artist": "John Severin Brassell",
+    "artist_ids": [
+        "a4f50310-4b01-4cd3-8ab8-85b31e6876db"
+    ],
+    "illustration_id": "f2a7285c-2ba8-483a-bf52-1fd05fe73476",
+    "border_color": "black",
+    "frame": "2015",
+    "frame_effects": [
+        "showcase"
+    ],
+    "security_stamp": "oval",
+    "full_art": false,
+    "textless": false,
+    "booster": false,
+    "story_spotlight": false,
+    "promo_types": [
+        "boosterfun"
+    ],
+    "edhrec_rank": 4828,
+    "prices": {
+        "usd": "0.52",
+        "usd_foil": "1.52",
+        "usd_etched": null,
+        "eur": "0.69",
+        "eur_foil": "3.06",
+        "tix": null
+    },
+    "related_uris": {
+        "tcgplayer_infinite_articles": "https://partner.tcgplayer.com/c/4931599/1830156/21018?subId1=api&trafcat=tcgplayer.com%2Fsearch%2Farticles&u=https%3A%2F%2Fwww.tcgplayer.com%2Fsearch%2Farticles%3FproductLineName%3Dmagic%26q%3DBonecrusher%2BGiant%2B%252F%252F%2BStomp",
+        "tcgplayer_infinite_decks": "https://partner.tcgplayer.com/c/4931599/1830156/21018?subId1=api&trafcat=tcgplayer.com%2Fsearch%2Fdecks&u=https%3A%2F%2Fwww.tcgplayer.com%2Fsearch%2Fdecks%3FproductLineName%3Dmagic%26q%3DBonecrusher%2BGiant%2B%252F%252F%2BStomp",
+        "edhrec": "https://edhrec.com/route/?cc=Bonecrusher+Giant"
+    },
+    "purchase_uris": {
+        "tcgplayer": "https://partner.tcgplayer.com/c/4931599/1830156/21018?subId1=api&u=https%3A%2F%2Fwww.tcgplayer.com%2Fproduct%2F199232%3Fpage%3D1",
+        "cardmarket": "https://www.cardmarket.com/en/Magic/Products?idProduct=401229&referrer=scryfall&utm_campaign=card_prices&utm_medium=text&utm_source=scryfall",
+        "cardhoarder": "https://www.cardhoarder.com/cards?affiliate_id=scryfall&data%5Bsearch%5D=Bonecrusher+Giant&ref=card-profile&utm_campaign=affiliate&utm_medium=card&utm_source=scryfall"
+    }
+} as Card;
