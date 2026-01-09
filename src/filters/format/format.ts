9bef9d34ad74b12ad46724b7d9bd8212b1735f74
Daniel Soskey <asks@danielsoskey.com>
2026-01-09 08:14:45 -0800

    add missing formats including premodern

--- src/filters/format/format.ts
+++ src/filters/format/format.ts
@@ -6,6 +6,7 @@ import { oracleNode } from '../oracle'
 export const FORMATS = [
   "standard" , "future" , "historic" , "pioneer" , "modern" , "legacy" , "paupercommander" ,
   "pauper" ,"vintage" , "penny" , "commander" , "brawl" , "duel" , "oldschool" , "timeless",
+  'premodern', 'predh', 'gladiator', 'oathbreaker', 'standardbrawl', 'alchemy',
 ]
 
 export const formatMatch = (legality: Legality, value: string): FilterNode => oracleNode({
